ffde673551e1bf58cbd1482f218b48d1a802b779
David Bazile <dbazile@bericotechnologies.com>
2015-05-27 15:31:44 -0400

    Updated config

---
+++ mimosa-config.coffee
@@ -0,0 +1,9 @@
+exports.config =
+  modules: ['jshint', 'copy']
+  watch:
+    sourceDir: 'src'
+    compiledDir: 'lib'
+    javascriptDir: null
+  jshint:
+    rules:
+      node: true
--- src/config.js
+++ src/config.js
@@ -1,24 +1,20 @@
-/* eslint strict:0 */
-"use strict";
-
-//var path = require( "path" ),
-//    fs = require( "fs" );
+'use strict';
 
 exports.defaults = function() {
   return {
     svgstore: {
-      inDir: "svg/icons",
-      outDir: "svg",
-      options: {}
+      sourcePattern: 'svg/**/*.svg',
+      outputFile:    'svg/repository.html'
     }
   };
 };
 
-exports.validate = function ( config, validators ) {
+exports.validate = function(config, validators) {
   var errors = [];
 
-  if ( validators.ifExistsIsObject( errors, "svgstore config", config.svgstore ) ) {
-
+  if (validators.isObject(errors, 'svgstore config', config.svgstore)) {
+    validators.isString(errors, 'svgstore.sourcePattern', config.svgstore.sourcePattern);
+    validators.isString(errors, 'svgstore.outputFile', config.svgstore.outputFile);
   }
 
   return errors;
