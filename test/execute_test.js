bb343c9aae400005a71850198a95048f8a8ec3e4
David Bazile <davidbazile@gmail.com>
2015-05-27 23:54:01 -0400

    Stubbed out `execute` test

--- test/execute_test.js
+++ test/execute_test.js
@@ -1,45 +1,79 @@
-var path = require( "path" )
-  , fs = require( "fs" )
-  , mod = require( path.join( process.cwd(), "src", "index" ) )
-  , generateSvg = mod.generateSvg
-  ;
+var path    = require('path');
+var expect  = require('chai').expect;
+var execute = require('../src/execute');
 
-describe("when generateSvg is invoked", function() {
+const SOURCE_PATTERN = 'fixtures/input/**/*.svg';
+const OUTPUT_FILE    = 'fixtures/output/repository.html';
+const REPOSITORY_ID  = 'test-repo-id';
+const VIEW_BOX       = '0 0 9001 9001';
 
-  it("will output nothing if there are no .svg files", function(){
-    generateSvg({});
+describe('execute#main()', function () {
+  var config, logger;
+
+  beforeEach(function() {
+    config = generateConfig();
+    logger = generateLogger();
   });
 
+  it('operates normally', function () {
+    execute(config, logger);
+    console.log(logger.playback());
+    expect(true).to.be.false;
+  });
 
-  describe("when creating SVG output", function() {
+  it('can handle not finding files', function () {
+    expect(true).to.be.false;
+  });
 
-    describe("with one folder", function() {
+  it('handles output file in unwriteable directory', function () {
+    expect(true).to.be.false;
+  });
 
-      it("will create proper output .svg with one input file", function() {
-        var mimosaConfig ={
-
-        };
-
-        generateSvg({});
-
-
-      });
-
-      it("will create proper output .svg with multiple input files", function() {
-
-      });
-
-    });
-
-
-    describe("with multiple folders", function() {
-
-      it("will create proper output .svg with one input file", function() {
-
-      });
-
-    });
-
+  it('handles bad XML gracefully', function () {
+    expect(true).to.be.false;
   });
 
 });
+
+///
+/// Helpers
+///
+
+function generateConfig() {
+  return {
+    sourcePattern: SOURCE_PATTERN,
+    outputFile:    OUTPUT_FILE,
+    repositoryId:  REPOSITORY_ID,
+    viewBox:       VIEW_BOX
+  };
+}
+
+function generateLogger() {
+  return {
+    _entries: [],
+    _record: function (op, _arguments) {
+      this._entries.push({
+        op: op,
+        args: [].slice.call(_arguments)
+      });
+    },
+    debug: function () {
+      this._record('debug', arguments);
+    },
+    info: function () {
+      this._record('info', arguments);
+    },
+    log: function () {
+      this._record('log', arguments);
+    },
+    playback: function () {
+      return this._entries.slice();
+    },
+    success: function () {
+      this._record('success', arguments);
+    },
+    warn: function () {
+      this._record('warn', arguments);
+    }
+  };
+}
