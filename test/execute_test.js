7e2349e9433f891af693e8c81f5e60be74043d1d
David Bazile <davidbazile@gmail.com>
2015-05-28 01:36:49 -0400

    Added test infrastructure

--- test/execute_test.js
+++ test/execute_test.js
@@ -1,24 +1,24 @@
+var fs      = require('fs');
 var path    = require('path');
 var expect  = require('chai').expect;
 var execute = require('../src/execute');
 
-const SOURCE_PATTERN = 'fixtures/input/**/*.svg';
-const OUTPUT_FILE    = 'fixtures/output/repository.html';
+const SOURCE_PATTERN = 'test/fixtures/input/**/*.svg';
+const OUTPUT_FILE    = 'test/fixtures/output/test-repository.html';
 const REPOSITORY_ID  = 'test-repo-id';
 const VIEW_BOX       = '0 0 9001 9001';
 
 describe('execute#main()', function () {
   var config, logger;
 
-  beforeEach(function() {
+  beforeEach(function setUp() {
     config = generateConfig();
     logger = generateLogger();
+    removeOutputFile();
   });
 
-  it('operates normally', function () {
-    execute(config, logger);
-    console.log(logger.playback());
-    expect(true).to.be.false;
+  afterEach(function tearDown() {
+    removeOutputFile();
   });
 
   it('can handle not finding files', function () {
@@ -38,6 +38,10 @@ describe('execute#main()', function () {
 ///
 /// Helpers
 ///
+
+function _onlyErrors(log) {
+  return log.op === 'error';
+}
 
 function generateConfig() {
   return {
@@ -60,6 +64,9 @@ function generateLogger() {
     debug: function () {
       this._record('debug', arguments);
     },
+    error: function () {
+      this._record('error', arguments);
+    },
     info: function () {
       this._record('info', arguments);
     },
@@ -76,4 +83,10 @@ function generateLogger() {
       this._record('warn', arguments);
     }
   };
+}
+
+function removeOutputFile() {
+  try {
+    fs.unlinkSync(OUTPUT_FILE);
+  } catch(e) {}
 }
